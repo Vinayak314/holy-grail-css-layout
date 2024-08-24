79737c71f08a5b9b0e7e1c5fe94f61c5fbbe9d13
Vinayak314 <vinayakpai2006@gmail.com>
2024-08-24 22:00:33 +0530

    Finish Project

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleFunct() {
+  const extnd = document.querySelector(".outer-grid");
+  extnd.classList.toggle("outer-grid-extended");
+
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("hide-nav");
+}
